e559067a20f24f80dc7485580e87713403c2aff9
AnthonyA490 <113041244+AnthonyA490@users.noreply.github.com>
2022-09-20 10:53:27 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,23 @@
-
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber == num_) {
+        basic.showIcon(IconNames.Yes)
+    }
+    if (receivedNumber != num_) {
+        basic.showIcon(IconNames.No)
+    }
+})
+input.onButtonPressed(Button.A, function () {
+    num_ += 1
+    basic.showNumber(num_)
+})
+input.onButtonPressed(Button.B, function () {
+    num_ += -1
+    basic.showNumber(num_)
+})
+input.onGesture(Gesture.Shake, function () {
+    radio.sendNumber(num_)
+})
+let num_ = 0
+basic.showIcon(IconNames.Angry)
+radio.setGroup(20)
+num_ = 2
